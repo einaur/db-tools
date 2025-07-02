15b038090d2afd5bf51a15b840ff376cc212094b
einaur <eanorway@gmail.com>
2025-07-02 23:32:26 +0200

    add API

---
+++ db_tools/api.py
@@ -0,0 +1,28 @@
+from db_tools.db import get_db_connection, fetch_inputs
+from db_tools.search import find_filenames_by_subset_inputs
+
+
+class DBTools:
+    def __init__(self, prefix="output"):
+        self.prefix = prefix
+        self.base_filters = {}
+
+    def set_base_filters(self, **kwargs):
+        self.base_filters = kwargs
+
+    def search(self, **filters):
+        all_filters = self.base_filters.copy()
+        all_filters.update(filters)
+
+        db_path = f"{self.prefix}.db"
+        conn = get_db_connection(db_path)
+        results = find_filenames_by_subset_inputs(all_filters, conn)
+        conn.close()
+        return [filename for filename, inputs, _ in results]
+
+    def get_inputs(self, fileroot):
+        db_path = f"{self.prefix}.db"
+        conn = get_db_connection(db_path)
+        inputs = fetch_inputs(conn, fileroot)
+        conn.close()
+        return inputs
